f0d1f1bb7c6f8b8f1ff7e6abdd54f8820bab13a3
Ayrton <ayrton.noble@gmail.com>
2025-09-16 12:17:25 +0100

    first commit

---
+++ day 1/print.py
@@ -0,0 +1,20 @@
+# print("hello world") --- print function intro
+
+# print("hello world\nhello world") --- print on new line
+
+# print("hello" + " " + "Ayrton") - adding multiple strings in print function
+
+# print("Hello " + input("What is your name? ") + "!") - have user type in their name and add it to string
+
+# name = input("What is your name? ") - anther method of above
+# print("Hello " + name + "!")
+
+# print(len(input("What is your name? "))) - print length of user input - 1 line
+# name = input("What is your name? ") - print length of user input - 3 lines
+# length = len(name)
+# print(length)
+
+# print("Welcome to the band name generator.")
+# city = input("What city did you grow up in?\n")
+# pet = input("What is the name of a pet?\n")
+# print("Your band name could be " + city + " " + pet)
